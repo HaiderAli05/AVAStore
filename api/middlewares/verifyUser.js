9a9ce355b7dd6a248e1cb651eb4ff0be9cacf0ad
Haider Ali <haider.ali3737@gmail.com>
2021-04-28 05:10:48 +0500

    change key name from authToken to token

--- api/middlewares/verifyUser.js
+++ api/middlewares/verifyUser.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req,res,next){
-    const token = req.header('authToken') ;
+    const token = req.header('token') ;
     if(!token) return res.status(401).json({message: 'UnAuthorized request.'});
     try {
         const verified = jwt.verify(token, process.env.USER_TOKEN);
